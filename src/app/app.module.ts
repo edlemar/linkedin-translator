feaebafebc0e63fa5fc3c792673c5b7f486172d5
Eduardo Lerin <eduardolerin@Eduardos-MacBook-Pro.local>
2023-01-06 00:07:37 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
 
@@ -29,7 +28,7 @@ import { FormsModule } from '@angular/forms';
     DragDropModule,
     FormsModule
   ],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
